23e833a6c0f41fdd08e0b7574e9e70893047cf99
Jorge-Rodriguez-Folguera <thebigjerboyt77@gmail.com>
2024-02-21 19:00:05 +0100

    a

--- index.php
+++ index.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Tienda Online</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
 
-  <body>
-    <div class="parent">
-      <div class="menuTop">
-        <div class="search-container">
-          <input class="buscar" type="text" placeholder="Buscar..." />
-          <button type="submit">Buscar</button>
-        </div>
-        <div class="log_in">
-          <a href="./login.html">
-            <h3>LOG IN</h3>
-          </a>
-        </div>
+<head>
+  <meta charset="UTF-8" />
+  <title>Tienda Online</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <div class="parent">
+    <div class="menuTop">
+      <div class="search-container">
+        <input class="buscar" type="text" placeholder="Buscar..." />
+        <button type="submit">Buscar</button>
       </div>
-      <div class="body">
-        <div class="inventario">
-          <h1>PRODUCTOS</h1>
-          <div class="productos"></div>
-        </div>
+      <a class="carrito" href="./carrito.php"><img src="carrito-de-compras.png" alt="carrito" /></a>
+      <div class="log_in">
+        <a href="./login.php">
+          <h3>LOG IN</h3>
+        </a>
       </div>
     </div>
+    <div class="body">
+      <div class="inventario">
+        <h1>PRODUCTOS</h1>
+        <div class="productos"></div>
+      </div>
+    </div>
+  </div>
 
-    <script src="script.js"></script>
-  </body>
+  <script src="script.js"></script>
+</body>
+
 </html>
--- login.php
+++ login.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Tienda Online</title>
-    <link rel="stylesheet" href="loginStyle.css" />
-  </head>
-  <body>
-    <div class="parent">
-      <div class="menuTop">
-        <div class="log_in">
-          <a href="./index.html">
-            <h3>Inicio</h3>
-          </a>
-        </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Tienda Online</title>
+  <link rel="stylesheet" href="loginStyle.css" />
+</head>
+
+<body>
+  <div class="parent">
+    <div class="menuTop">
+      <div class="log_in">
+        <a href="./index.php">
+          <h3>Inicio</h3>
+        </a>
       </div>
-      <div class="body">
-        <div class="form">
-          <label for="nombre">Nombre</label>
-          <input type="text" name="nombre" id="nombre" />
-          <label for="password">Contraseña</label>
-          <input type="text" name="password" id="password" />
-          <div>
-            <button onclick="cancelar()" type="submit">Cancelar</button>
-            <button onclick="registrar()" type="submit">Aceptar</button>
-          </div>
+    </div>
+    <div class="body">
+      <div class="form">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" id="nombre" />
+        <label for="password">Contraseña</label>
+        <input type="text" name="password" id="password" />
+        <div>
+          <button onclick="cancelar()" type="submit">Cancelar</button>
+          <button onclick="registrar()" type="submit">Aceptar</button>
         </div>
       </div>
     </div>
-    <script src="script.js"></script>
-  </body>
+  </div>
+  <script src="script.js"></script>
+</body>
+
 </html>
